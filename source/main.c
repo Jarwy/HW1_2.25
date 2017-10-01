61e82b6fa7df22fc76e945b85df9a1dfd5d35a03
Jarwy <jarwy880413@gmail.com>
2017-10-01 21:07:49 +0800

    first commit

---
+++ source/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf(" HHHHHHHHHHHHH\n");
+	printf("       H\n");
+	printf("       H\n");
+	printf("       H\n");
+	printf("       H\n");
+	printf("       H\n");
+	printf(" HHHHHHHHHHHHH\n\n\n");
+
+	printf("    CCCCCCC \n");
+	printf("   C       C\n");
+	printf("  C         C\n");
+	printf(" C           C\n");
+	printf(" C           C\n");
+	printf(" C           C\n");
+	printf(" C           C\n\n\n");
+
+	printf("WWWWWWWWWWWWWW\n");
+	printf(" WW     \n");
+	printf("   WWW    \n");
+	printf("      WWWW    \n");
+	printf("   WWW  \n");
+	printf(" WW      \n");
+	printf("WWWWWWWWWWWWWW\n\n\n");
+
+	system("pause");
+	return 0;
+}
